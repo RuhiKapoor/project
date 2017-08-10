7d5d90f4fb916293d9ee62a140f5bdf4ddcc4a98
RuhiKapoor <30615937+RuhiKapoor@users.noreply.github.com>
2017-08-10 11:09:08 -0700

    Add files via upload

---
+++ characterinput.py
@@ -0,0 +1,4 @@
+a=input("enter your age:")
+b=100-a
+c=2017+b
+print 'you will be 100 in',c
---
+++ commonelements.py
@@ -0,0 +1,5 @@
+list=[1,23,3,4,5]
+alist=[1,2,3]
+for elements in list :
+    if elements in alist :
+        print (elements)
