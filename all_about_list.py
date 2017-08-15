8c5ebe4d4f33dfed640a10ff0a2e3bb4834ab046
RuhiKapoor <30615937+RuhiKapoor@users.noreply.github.com>
2017-08-15 10:10:09 -0700

    Add files via upload

---
+++ all_about_list.py
@@ -0,0 +1,21 @@
+list=['abcd',786,2.23,'join',70.2]
+print list[0]
+print list[1:3]
+print list[2:]
+print list*2
+print list+list*3
+print list[::]
+print list[::-1]
+print list[:2:-1]
+print list[:-2]
+my_list=['hello',[1,2,3],'bye']
+print my_list
+list.append('brother')
+print list
+list.extend('sister')
+print list
+list.insert(1,'hello')
+print list
+list[2]=[198,112]
+print list
+list[1]
